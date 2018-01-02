0a3851be6d64097a7c07ef0c5e50fed246f47665
Katsuhiko YOSHIDA <claddvd@gmail.com>
2018-01-02 13:56:42 +0900

    Update version

--- tosa.rb
+++ tosa.rb
@@ -1,14 +1,14 @@
 class Tosa < Formula
   desc "Open Pull request page from commit hash tool"
   homepage "https://github.com/kyoshidajp/tosa"
-  version '0.0.1'
+  version '0.0.2'
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/kyoshidajp/tosa/releases/download/0.0.1/tosa_darwin_amd64.zip"
-    sha256 "dcfd4dfd9e2d583fe29e24288d0f932b812ab41d399d1eb9a926ea760b5a6374"
+    url "https://github.com/kyoshidajp/tosa/releases/download/0.0.2/tosa_darwin_amd64.zip"
+    sha256 "c74bc6c1834244b0ca32b37a674cc462888615393aee78775a5e928dd95e96d9"
   else
-    url "https://github.com/kyoshidajp/tosa/releases/download/0.0.1/tosa_darwin_386.zip"
-    sha256 "1a2d484a811952031d29e704b35ac33372bf7d975e31d908550cb7f4c3a0ee1e"
+    url "https://github.com/kyoshidajp/tosa/releases/download/0.0.2/tosa_darwin_386.zip"
+    sha256 "63aea7ec3ac84871cd22e443c0261d6f0ce96f574c41d6998238ba39bbb9d92b"
   end
 
   def install
