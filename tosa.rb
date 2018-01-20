a87ef931fc8cd2780deed86347764ba5faef873c
Katsuhiko YOSHIDA <claddvd@gmail.com>
2018-01-21 00:58:28 +0900

    Fix release file directory version

--- tosa.rb
+++ tosa.rb
@@ -4,10 +4,10 @@ class Tosa < Formula
   version 'v0.0.3'
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/kyoshidajp/tosa/releases/download/0.0.2/tosa_darwin_amd64.zip"
+    url "https://github.com/kyoshidajp/tosa/releases/download/v0.0.3/tosa_darwin_amd64.zip"
     sha256 "13c2767015cd2393da82e35ffae658377df2de05deb825a796b2bfaf8a716200"
   else
-    url "https://github.com/kyoshidajp/tosa/releases/download/0.0.2/tosa_darwin_386.zip"
+    url "https://github.com/kyoshidajp/tosa/releases/download/v0.0.3/tosa_darwin_386.zip"
     sha256 "93f276564a8d11393316315af83b89671b9e280226fe45e0d59d33615e788dc9"
   end
 
